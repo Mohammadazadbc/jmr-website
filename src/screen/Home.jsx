f34d9e23bb2d8a28ec10c3a9bc68b390d376d4e7
azad <mohammadazad.bc@gmail.com>
2022-05-07 20:47:00 +0200

    code source

---
+++ src/screen/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam facere at quam aspernatur commodi laborum recusandae aperiam atque officia molestiae rem dignissimos quibusdam, rerum illum? Aliquam molestiae blanditiis est cumque. </div>
+  )
+}
+
+export default Home
